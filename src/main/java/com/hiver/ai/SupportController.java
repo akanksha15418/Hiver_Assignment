83be82063ce2ebfb98a1d95e4f20f6bb541cfd3e
Akanksha Gupta <shreya.akanksha2773@gmail.com>
2026-09-12 00:30:39 +0530

    feat: setup CORS and dynamic URL for separated frontend/backend deployment

--- src/main/java/com/hiver/ai/SupportController.java
+++ src/main/java/com/hiver/ai/SupportController.java
@@ -4,8 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/support")
 public class SupportController {
